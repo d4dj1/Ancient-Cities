1a31305298b1323c700ebca948c0acd298b8b94c
d4dj1 <dwaynespence13@icloud.com>
2025-01-14 13:53:00 -0500

    Add files via upload

---
+++ CostCalculate/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    
+        private bool Calculating = true;
+        public void areaCalculator()
+        {
+            Console.WriteLine("Insert the name of the first Shape to be calculated.");
+            
+           string intShape = Console.ReadLine();
+        
+            while(Calculating)
+            {
+                
+
+                for(string Shape = "Square"; Shape <= 4; i++)
+                {
+                    
+                
+                    if(Shape = "Square")
+                    {
+                        
+                        Console.WriteLine("Insert the length of one side of the rectangle or square.");
+                
+                    }    int intSqLenght1 = int.Parse(Console.ReadLine());
+                    
+                     
+                    
+                    
+                    
+                                                                     
+                }                   
+           } 
+        
+        }       
+}
